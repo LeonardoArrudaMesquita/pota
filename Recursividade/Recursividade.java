179e8e20f9c813aeec9c6e54c27b2818d70f59bf
LeonardoArrudaMesquita <leonardoarruda@outlook.com.br>
2019-08-18 18:28:11 -0300

    Exercício 1 e 2, menu dinâmico

--- Recursividade/Recursividade.java
+++ Recursividade/Recursividade.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -9,12 +10,49 @@ public class Recursividade {
         
         Scanner sc = new Scanner(System.in);
 
+        do {
+            
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("Escolha uma operação: ");
+            sb.append("1 - Potência");
+            sb.append("2 - Fatorial");
+
+        } while (opcao != 0);
+
         // Exercício 1 
-        System.out.println("Digite uma Base e um Expoente: ");
-        System.out.println(potencial(sc.nextInt(), sc.nextInt()));
+        // System.out.println("Digite uma Base e um Expoente: ");
+        // System.out.println("Potência: " + potencia(sc.nextInt(), sc.nextInt()));
+
+        // Exercício 2
+        System.out.println("Digite uma número para o fatorial: ");
+        System.out.println("Fatorial: " + fatorial(sc.nextInt()));
     }
 
-    public static int potencial(int base, int expoente){
-        return 1;
+    // Exercício 1
+    public static double potencia(int base, int expoente)
+    {        
+        if (expoente == 0) 
+        {
+            return 1;
+        }
+        else if (expoente < 0) 
+        {
+            return 1 / base * potencia(base, -(-expoente - 1));
+        }
+        else
+        {
+            return base * potencia(base, expoente - 1);            
+        }        
     }
+
+    // Exercício 2
+    public static int fatorial(int n)
+    {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * fatorial(n - 1);
+    }    
 }
