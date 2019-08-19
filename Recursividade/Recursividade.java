4ea8ae1bd37422efab594ffca129afdf77e42109
LeonardoArrudaMesquita <leonardoarruda@outlook.com.br>
2019-08-18 22:55:47 -0300

    Exercício 3 e 4 cinquenta por cento

--- Recursividade/Recursividade.java
+++ Recursividade/Recursividade.java
@@ -6,27 +6,55 @@ import java.util.Scanner;
  */
 public class Recursividade {
 
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();        
+        
+        sb.append("Escolha uma operação:\n");
+        sb.append("1 - Potência\n");
+        sb.append("2 - Fatorial\n");
+        sb.append("3 - Fibonacci\n");
+        sb.append("4 - Maior elemento em um Array\n");
+        sb.append("0 - Sair\n");
+        sb.append("-----------------------------------");
+
+        int opcao;
 
         do {
+            System.out.println(sb);
+            opcao = sc.nextInt();    
             
-            StringBuilder sb = new StringBuilder();
+            switch (opcao) {
+                
+                // Exercício 1 
+                case 1:                    
+                    System.out.println("Digite uma Base e um Expoente: ");
+                    System.out.println("Potência: " + potencia(sc.nextInt(), sc.nextInt()));
+                    break;
+            
+                // Exercício 2
+                case 2:                    
+                    System.out.println("Digite o fatorial: ");
+                    System.out.println("Fatorial: " + fatorial(sc.nextInt()));
+                
+                case 3:
+                    System.out.println("Digite o nº termo: ");
+                    System.out.println("Fibonacci: " + fibonacci(sc.nextInt()));
 
-            sb.append("Escolha uma operação: ");
-            sb.append("1 - Potência");
-            sb.append("2 - Fatorial");
+                case 4:
+                    System.out.println("Digite o tamanho do Array: ");
+                    System.out.println("Maior elemento: " + maiorElemento(sc.nextInt()));
 
-        } while (opcao != 0);
+                // Sair
+                case 0:                
+                    System.out.println("Saindo...");
 
-        // Exercício 1 
-        // System.out.println("Digite uma Base e um Expoente: ");
-        // System.out.println("Potência: " + potencia(sc.nextInt(), sc.nextInt()));
-
-        // Exercício 2
-        System.out.println("Digite uma número para o fatorial: ");
-        System.out.println("Fatorial: " + fatorial(sc.nextInt()));
+                default:
+                    break;                
+            }
+        } while (opcao != 0);        
     }
 
     // Exercício 1
@@ -54,5 +82,41 @@ public class Recursividade {
         }
 
         return n * fatorial(n - 1);
-    }    
+    }
+
+    static int anterior = 0;
+    static int atual = 0;
+    static int soma = 0;
+
+    // Exercício 3
+    public static String fibonacci(int n){
+        
+        if(n == 0){
+            return "";
+        }               
+        
+        if (atual == 0) {
+            atual = 1;
+            return "0, 1, " + fibonacci(--n);
+        }
+        
+        soma = anterior + atual;                
+        anterior = atual;        
+        atual = soma;
+
+        return soma + ", " + fibonacci(n - 1); 
+    }
+    
+    // Exercício 4
+    public static int maiorElemento(int tamanho){        
+
+        int[] arr = new int[tamanho];
+
+       if(arr[arr.length] == "" ){
+            for (int i = 0; i < tamanho; i++) {
+                arr[i] = sc.nextInt();
+            }
+       }
+
+    }
 }
